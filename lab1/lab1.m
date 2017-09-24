14955b030b6af8e7a2faee81e82d19d9d6412d27
0merjavaid <14besemjavaid@seecs.edu.pk>
2017-09-24 14:36:24 +0500

    lab2

---
+++ lab1/lab1.m
@@ -0,0 +1,30 @@
+% Name :Muhammad Umer Javaid
+% Regno: 111238
+% Class: Bese5a
+
+
+
+
+%TAsk one is to show image in matlab using differnet commands
+imshow('images.jpg');
+img = imread('images.jpg');
+imagesc(img);
+
+subplot (1,1,1), imshow('images.jpg');
+%using different color map to display image
+image = imread('images.jpg'); 
+colormap(image,winter)
+
+
+%Task two is to extract 3 channels from a color image
+img = imread('images.jpg');
+red=im(:,:,1);
+green=im(:,:,2);
+blue=im(:,:,3);
+imshow(red);
+
+%Task 3 is to resize image and save as bmg format
+img=imread('images.jpg');
+img1=imresize(im,0.4);
+ 
+imwrite(img1,'bmpfile.bmp');
